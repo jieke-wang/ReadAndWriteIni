3519240166c15593115f4889c5fa150e9f55ff24
yihu wang <wangyihu2009@msn.com>
2021-07-24 11:52:14 +0800

    first commit

---
+++ ReadAndWriteIni/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ReadAndWriteIni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Current;
+            Current = Directory.GetCurrentDirectory();//获取当前根目录
+            Console.WriteLine("Current directory {0}", Current);
+            // 写入ini
+            Ini ini = new Ini(Current + "/config.ini");
+            ini.Write("Setting", "key1", "hello word!");
+            ini.Write("Setting", "key2", "hello ini!");
+            ini.Write("SettingImg", "Path", "IMG.Path");
+            // 读取ini
+            string stemp = ini.ReadValue("Setting", "key2");
+            Console.WriteLine(stemp);
+
+            Console.ReadKey();
+        }
+    }
+}
